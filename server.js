f88c8f0716f4a8dde26f001f4a3a21b979ef854d
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-08-09 23:20:20 -0400

    fixed route

--- server.js
+++ server.js
@@ -3,19 +3,34 @@
 
 //dependencies
 var express = require('express');
-
-
-//var definitions
 var app = express();
-var port = 8080;
+
+
+
+//defining the variables
+var port = 3000;
 var message = 'Hello Worldah!'
 
 
-//routes
-app.get('/', function(req, res){
+
+//routes for the API
+//var router = express.Router();	//get an instance of the express Router
+
+//test route
+//router.get('/', function(req, res){
+//	res.send(message);
+	//res.json({ message: 'horray! welcome to our api!'});
+//});
+
+//registering the ROUTES for the api prefix
+//app.use('/api', router);
+
+
+
+app.get('/', function(req,res){
 	res.send(message);
 });
 
-
-
+//starting the server
 app.listen(port);
+console.log('Magic is happening at http://localhost:' + port + '!');
