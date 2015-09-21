00af7f32a69765e1f8945df315fabc6ba6560d6d
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2015-09-20 22:08:45 -0400

    finished UI

--- clientConsumer/Angular/js/app.js
+++ clientConsumer/Angular/js/app.js
@@ -3,8 +3,7 @@ angular.module("shApp", [])
 
 .controller('mainController', function ($scope) {
 
-	$scope.selectedMethod = 'GET';
-
+	$scope.method = 'get';
 
 	$scope.methods = [
 		{name: 'GET', method: 'get'},
@@ -13,4 +12,71 @@ angular.module("shApp", [])
 		{name: 'DELETE', method: 'delete'}
 	];
 
-})
+	//initial state setup
+	$scope.showGet = '';
+	$scope.showPost = 'ng-hide';
+	$scope.showPut = 'ng-hide';
+	$scope.showDelete = 'ng-hide';
+
+	//changes show/hide attributes based on selection
+	$scope.showMethod = function () {
+
+		if ($scope.method == 'get') {
+			$scope.showGet = '';
+			$scope.showPost = 'ng-hide';
+			$scope.showPut = 'ng-hide';
+			$scope.showDelete = 'ng-hide';
+		};
+
+
+		if ($scope.method == 'post') {
+			$scope.showGet = 'ng-hide';
+			$scope.showPost = '';
+			$scope.showPut = 'ng-hide';
+			$scope.showDelete = 'ng-hide';
+		};
+
+		if ($scope.method == 'put') {
+			$scope.showGet = 'ng-hide';
+			$scope.showPost = 'ng-hide';
+			$scope.showPut = '';
+			$scope.showDelete = 'ng-hide';
+		};
+
+		if ($scope.method == 'delete') {
+			$scope.showGet = 'ng-hide';
+			$scope.showPost = 'ng-hide';
+			$scope.showPut = 'ng-hide';
+			$scope.showDelete = '';
+		};
+	}
+
+});
+
+
+
+	// if $scope.method is changed
+
+	// if ($scope.method == 'get') {
+	// 	$scope.showPost = 'ng-hide';
+	// 	$scope.showPut = 'ng-hide';
+	// 	$scope.showDelete = 'ng-hide';
+	// };
+
+	// if ($scope.method == 'post') {
+	// 	$scope.showGet = 'ng-hide';
+	// 	$scope.showPut = 'ng-hide';
+	// 	$scope.showDelete = 'ng-hide';
+	// };
+
+	// if ($scope.method == 'put') {
+	// 	$scope.showGet = 'ng-hide';
+	// 	$scope.showPost = 'ng-hide';
+	// 	$scope.showDelete = 'ng-hide';
+	// };
+
+	// if ($scope.method == 'delete') {
+	// 	$scope.showGet = 'ng-hide';
+	// 	$scope.showPost = 'ng-hide';
+	// 	$scope.showPut = 'ng-hide';
+	// };
