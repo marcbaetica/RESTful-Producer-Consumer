e2e2e81c3e931aea0ba6ad209258eaeffd12c9c7
Marc-Alexandru Baetica <marc.baetica@gmail.com>
2016-01-19 14:43:13 -0500

    updated server.js

--- server.js
+++ server.js
@@ -94,3 +94,71 @@ app.get('/', function (req, res) {
 app.listen(app.get('port'), function(){
 	console.log('Magic is happening at http://' + app.get('dest') + ":" + app.get('port') + " !!!");
 });
+
+
+
+/*
+
+FOR TESTING PURPOSES
+
+var request = require('request').defaults({jar: true});
+var prettyjson = require('prettyjson');
+​
+var url = 'http://localhost:10643/WebClient/rest';
+var jar = request.jar();
+var jsonHeader = {"content-type": "application/json"};
+​
+//Query object to send
+var query = {'InventorySearchFacetRequest': {
+    'filter': [
+        {
+            'field': 'vendorName',
+            'value': 'CentOS'
+        },
+        {
+            'field': 'productName',
+            'value': 'kernel'
+        }
+    ],
+    'size': 5,
+    'freeFormQuery': '*',
+    'searchIndex': 'inventory',
+    'aggregationList': [
+        'vendorName',
+        'computerName'
+    ]
+}};
+​
+//Test function
+function test() {
+	//Credentials to log into DSM
+	var credentials = '{"dsCredentials":{"userName":"admin","password":"admin"}}';
+	//Login with a post request to /rest/authentication/login
+	request.post({url: url + '/authentication/login', body: credentials, headers: jsonHeader},
+			function(error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log(body);
+			//If login succeeds, use sID from login to query /rest/softwareInventory
+			var inventoryUrl = url + '/softwareInventory';
+			
+			//sID is needed in a cookie for the query
+			var cookie = request.cookie('sID=' + body);
+			jar.setCookie(cookie, inventoryUrl);
+			
+			//Send the query
+			request.post({url: inventoryUrl, json: true, body: query, jar: jar},
+					function(error, response, body) {
+				//Pretty print the response
+				console.log(prettyjson.render(body, {numberColor: 'cyan'}));
+			});
+			//Log out with a request to /rest/authentication/logout
+			request.del({url: url + '/authentication/logout?sID=' + body});
+		} else {
+			//If login fails, print the response for debugging purposes
+			console.log(response);
+		}
+	});
+}
+test();
+
+*/
